356c4e3f385b7a9e73d56ab6d96f09d3da8f2a37
Chirstopher Hunter Fortune <CHFortune@fullsail.edu>
2019-02-12 20:14:16 -0500

    Added check to make sure game data is entered before saving.

--- GameTrack/app/src/main/java/com/fullsail/christopherfortune/gametrack/AddGameFragment/AddGameFragment.java
+++ GameTrack/app/src/main/java/com/fullsail/christopherfortune/gametrack/AddGameFragment/AddGameFragment.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.fullsail.christopherfortune.gametrack.R;
 
@@ -89,12 +90,47 @@ public class AddGameFragment extends Fragment {
         // If the user pressed the save_game_menu_button
         if(item.getItemId() == R.id.save_game_menu_button){
 
-            // Call the saveGame interface method
-            addGameFragmentInterfaceListener.saveGame(gameTitleEditText,
-                    gameDeveloperEditText,
-                    gamePublisherEditText,
-                    gameReleaseYearEditText,
-                    gameLauncherEditText);
+            // If the game title is empty
+            if(gameTitleEditText.getText().toString().trim().isEmpty()){
+
+                // Ask the user to enter in a game title
+                Toast.makeText(getContext(), "Please enter game title.", Toast.LENGTH_SHORT).show();
+
+            // if the game dev is empty
+            } else if (gameDeveloperEditText.getText().toString().trim().isEmpty()){
+
+                // Ask the user to enter in a game dev
+                Toast.makeText(getContext(), "Please enter game developer.", Toast.LENGTH_SHORT).show();
+
+            // If the game publisher is empty
+            } else if (gamePublisherEditText.getText().toString().trim().isEmpty()){
+
+                // Ask the user to enter in a game publisher
+                Toast.makeText(getContext(), "Please enter game publisher.", Toast.LENGTH_SHORT).show();
+
+            // If the game release year is empty
+            } else if (gameReleaseYearEditText.getText().toString().trim().isEmpty()){
+
+                // Ask the user to enter in a game release yer
+                Toast.makeText(getContext(), "Please enter game release year.", Toast.LENGTH_SHORT).show();
+
+            // If the game launcher is empty
+            } else if (gameLauncherEditText.getText().toString().trim().isEmpty()){
+
+                // Ask the user to enter in a game launcher
+                Toast.makeText(getContext(), "Please enter game launcher.", Toast.LENGTH_SHORT).show();
+
+            // If the user entered in data to all the edit text fields
+            } else {
+
+                // Call the saveGame interface method
+                addGameFragmentInterfaceListener.saveGame(gameTitleEditText,
+                        gameDeveloperEditText,
+                        gamePublisherEditText,
+                        gameReleaseYearEditText,
+                        gameLauncherEditText);
+            }
+
         }
 
         return true;
